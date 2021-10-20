c9a759d54e5530bf8cacbf556ce02afee4826d98
sagar <s.sawuck@providus.mu>
2021-10-20 11:40:40 +0400

    initial commit

--- src/components/ExchangeRate.js
+++ src/components/ExchangeRate.js
@@ -1,71 +1,43 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
+import React, { useCallback, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { ratesUpdated } from "../store/actions/RateActions";
-import {
-  getCurrencyCode,
-  getSupportedCurrencies,
-} from "../store/reducers/RateReducer";
-import { RateTableContainer } from "./RateTable";
-import { CurrencyCodePickerContainer } from "./CurrencyCodePicker";
+import { getCurrencyCode, getSupportedCurrencies, } from "../store/reducers/RateReducer";
+import { RateTable } from "./RateTable";
+import { CurrencyCodePicker } from "./CurrencyCodePicker";
 import { getExchangeRates } from "../api";
-import { AmountFieldContainer } from "./AmountField";
+import { AmountField } from "./AmountField";
 
-export class ExchangeRate extends React.Component {
-  constructor(props) {
-    super(props);
-    this.getLatestExchangeRates();
-  }
-  componentDidUpdate(prevProps) {
-    if (this.props.currencyCode !== prevProps.currencyCode) {
-      this.getLatestExchangeRates();
-    }
-  }
-  getLatestExchangeRates() {
-    const { currencyCode, updateRates, supportedCurrencies } = this.props;
+export default function ExchangeRate() {
+  useCurrencyCodes();
+  return (
+    <>
+      <section>
+        <h1 className="ExchangeRate-header">
+          Exchange Rates <CurrencyCodePicker />
+        </h1>
+      </section>
+      <section>
+        <AmountField />
+      </section>
+      <section>
+        <RateTable />
+      </section>
+    </>
+  );
+}
+
+function useCurrencyCodes() {
+  const dispatch = useDispatch();
+  
+  const supportedCurrencies = useSelector( getSupportedCurrencies);
+  const currencyCode = useSelector( getCurrencyCode);
+  const updateRates = useCallback((rates) => dispatch(ratesUpdated(rates)), [dispatch]);
+  
+  useEffect(getLatestExchangeRates, [currencyCode, supportedCurrencies, updateRates])
+  
+  function getLatestExchangeRates() {
     getExchangeRates(currencyCode, supportedCurrencies).then((rates) => {
       updateRates(rates);
     });
   }
-  render() {
-    return (
-      <>
-        <section>
-          <h1 className="ExchangeRate-header">
-            Exchange Rates <CurrencyCodePickerContainer />
-          </h1>
-        </section>
-        <section>
-          <AmountFieldContainer />
-        </section>
-        <section>
-          <RateTableContainer />
-        </section>
-      </>
-    );
-  }
 }
-
-// props types
-ExchangeRate.propTypes = {
-  updateCurrencyCode: PropTypes.func,
-  currencyCode: PropTypes.string,
-  supportedCurrencies: PropTypes.arrayOf(PropTypes.string),
-};
-
-// redux stuff
-function mapStateToProps(state) {
-  return {
-    supportedCurrencies: getSupportedCurrencies(state),
-    currencyCode: getCurrencyCode(state),
-  };
-}
-function mapDispatchToProps(dispatch) {
-  return {
-    updateRates: (rates) => dispatch(ratesUpdated(rates)),
-  };
-}
-export const ExchangeRateContainer = connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(ExchangeRate);
